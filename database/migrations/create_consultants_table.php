80d25cc34b0d2e5eab26bf1c9ee9655e9c69911d
danielhe4rt <danielhe4rt@gmail.com>
2025-10-30 15:41:16 -0300

    chore: add customizable labels and navigation settings for consultants module

--- database/migrations/create_consultants_table.php
+++ database/migrations/create_consultants_table.php
@@ -7,13 +7,15 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void
     {
-        $connection = (string)config('filament-consultants-module.consultants.connection');
+        $connection = (string)config('filament-consultants-module.consultants.database.connection');
+        $table = config('filament-consultants-module.consultants.database.tables.consultants', 'consultants');
 
-        if (!Schema::connection($connection)->hasTable('consultants')) {
-            Schema::connection($connection)->create(config('filament-consultants-module.consultants.database.tables.consultants', 'consultants'), function (Blueprint $table): void {
+        if (!Schema::connection($connection)->hasTable($table)) {
+
+            Schema::connection($connection)->create($table, function (Blueprint $table): void {
                 $table->id();
-                $table->string('provider');
-                $table->string('provider_id')->nullable();
+                $table->string('provider')->comment('CRM Provider');
+                $table->string('provider_id')->nullable()->comment('CRM Provider ID');;
                 $table->boolean('enabled');
                 $table->string('name');
                 $table->string('slug');
